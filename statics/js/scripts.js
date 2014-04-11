ce68fbfe41329edde20d5ff1bec2c523149a0ead
Jack Barham <jack@airboxmedia.com>
2014-04-11 16:36:43 +0100

    Update

--- statics/js/scripts.js
+++ statics/js/scripts.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function($){
     		$.each(data['hits']['hits'], function() {
     			console.log(this['_source']['productTitle']);
 
-    			thisResult =  '<div class="searchResultBox">';
+    			thisResult =  '<div class="sImg"><div class="searchResultBox"></div>';
     			thisResult += '<img src="http://www.argos.co.uk/' + this['_source']['imgUrl'] +  '"></img>'
     			thisResult += '<h3>' + this['_source']['productTitle'] + '</h3>';
     			thisResult += '<span>£' + this['_source']['price'] +  '</span>';

a6fd0124a868ff7e9fffdf0d258242060d1c8a58
Steven Frostwick <steven.frostwick@gmail.com>
2014-04-11 15:55:34 +0100

    efrtsydtufiyug

--- statics/js/scripts.js
+++ statics/js/scripts.js
@@ -8,7 +8,10 @@ jQuery(document).ready(function($){
     $('input.main-search').keyup(function() {
 
     	// Local Machine
-    	var searchURL = document.location.origin + '/search/*' + $(this).val() + '*';
+    	//var searchURL = document.location.origin + '/search/*' + $(this).val() + '*';
+
+    	// Stevens Machine
+    	var searchURL = 'http://192.168.68.141:6677/search/*' + $(this).val() + '*';
 
     	$.ajax({
     		url: searchURL

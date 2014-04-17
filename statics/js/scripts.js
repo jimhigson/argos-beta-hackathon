ce0753ec125a9bea239a3c626b2c64d7af5d2275
Jim Higson <jim.higson@gmail.com>
2014-04-17 12:31:22 +0100

    show results from URL on page load

--- statics/js/scripts.js
+++ statics/js/scripts.js
@@ -1,45 +1,36 @@
 /* ALL JQUERY
-   ========================================= */
+ ========================================= */
+
+$(document).ready(function ($) {
+
+   var searchBox = $('input.main-search');
    
-   
-jQuery.noConflict();
-jQuery(document).ready(function($){
+   function updateAutoComplete() {
 
-    $('input.main-search').keyup(function() {
+      if (searchBox.val()) {
+         var searchURL = '/find/' + searchBox.val();
 
-    	if($(this).val() !== '') {
-	    	var searchURL = document.location.origin + '/find/' + $(this).val();
+         $.ajax({
+            url: searchURL
+         }).done(function (data) {
+            $('#auto-complete').html('');
+            $.each(data['hits']['hits'], function () {
 
-	    	$.ajax({
-	    		url: searchURL
-	    	}).done(function(data) {
-	    		$('#auto-complete').html('');
-	    		$.each(data['hits']['hits'], function() {
-	    			console.log(this['_source']['productTitle']);
+               var source = this['_source'];
 
-	    			thisResult =  '<div class="searchResultBox cf">';
-	    			thisResult += '<div class="sImg"><img src="http://www.argos.co.uk/' + this['_source']['imgUrl'] +  '"></img></div>'
-	    			thisResult += '<h3>' + this['_source']['productTitle'] + '</h3>';
-	    			thisResult += '<span>£' + this['_source']['price'] +  '</span></div>';
+               thisResult = '<div class="searchResultBox cf">';
+               thisResult += '<div class="sImg"><img src="http://www.argos.co.uk/' + source['imgUrl'] + '"></img></div>'
+               thisResult += '<h3>' + source['productTitle'] + '</h3>';
+               thisResult += '<span>£' + source['price'] + '</span></div>';
 
-	    			//console.log(thisResult);
-	    			$('#auto-complete').append(thisResult);
-	    		});
-	    	});
-    	} else {
-    		$('#auto-complete').html('');
-    	}
+               $('#auto-complete').append(thisResult);
+            });
+         });
+      } else {
+         $('#auto-complete').html('');
+      }
+   }
 
-
-        $('#auto-complete').fadeIn('slow');
-
-    });
-
-    $('input.main-search').focusout(function() {
-
-        $('#auto-complete').fadeOut('slow');
-
-    });
-		
-
-}); /* end of as page load scripts */
+   searchBox.keyup(updateAutoComplete);
+   updateAutoComplete();
+});

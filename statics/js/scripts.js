8b3bc56cdeae3b0434a2c343544587afd2edced9
Jim Higson <jim.higson@gmail.com>
2014-04-25 10:58:20 +0100

    extract function

--- statics/js/scripts.js
+++ statics/js/scripts.js
@@ -6,6 +6,44 @@ $(document).ready(function ($) {
    var categories = $('#categories');
    var currentAjax = null;
 
+   function handleAjaxResult(data) {
+      results.html('');
+      data.hits.hits.forEach( function (hit) {
+
+         var productTitleHtml = (hit.highlight && hit.highlight.productTitle) || hit._source.productTitle,
+            productUrl = 'http://www.argos.co.uk/static/Product/partNumber/' + hit._source.productId + '.htm';
+
+         var hitHtml =    '<div class="searchResultBox">'
+            + '<img data-src="http://www.argos.co.uk/' + hit._source['imgUrl'] + '">'
+            + '<div class="description">'
+            + '<h3><a href="'+ productUrl +'">' + productTitleHtml + '</a></h3>'
+            + '<span>£' + Number(hit._source['price']).toFixed(2) + '</span>'
+            + '</div>'
+            + '</div>';
+
+         results.append(hitHtml);
+
+         currentAjax = null;
+      });
+      results.find('img').unveil();
+
+      categories.html('');
+
+      if( data.categories.length > 1 ) {
+         data.categories.forEach(function (cat) {
+            var catNameInUrl = makeInputUrlFriendly(cat.name),
+               catLink = '/search/' + catNameInUrl + '/' + queryTerm,
+               catHtml = '<span class="category">' +
+                  '<a href="' + catLink + '">' + cat.name + '</a>' +
+                  //'<button class="close">' +
+                  '</span>';
+
+            categories.append(catHtml);
+         });
+      }
+
+   }   
+   
    function sanitiseQueryTerm(term) {
       return term.trim().replace(/\s+/g, '_').replace(/\//g, '');
    }
@@ -28,43 +66,7 @@ $(document).ready(function ($) {
 
          currentAjax = $.ajax({
             url: searchURL
-         }).done(function (data) {
-            results.html('');
-            data.hits.hits.forEach( function (hit) {
-
-               var productTitleHtml = (hit.highlight && hit.highlight.productTitle) || hit._source.productTitle,
-                   productUrl = 'http://www.argos.co.uk/static/Product/partNumber/' + hit._source.productId + '.htm';
-               
-               var hitHtml =    '<div class="searchResultBox">'
-                                + '<img data-src="http://www.argos.co.uk/' + hit._source['imgUrl'] + '">'
-                                + '<div class="description">'
-                                   + '<h3><a href="'+ productUrl +'">' + productTitleHtml + '</a></h3>'
-                                   + '<span>£' + Number(hit._source['price']).toFixed(2) + '</span>'
-                                + '</div>'
-                             + '</div>';
-
-               results.append(hitHtml);
-
-               currentAjax = null;
-            });
-            results.find('img').unveil();
-
-            categories.html('');
-            
-            if( data.categories.length > 1 ) {
-               data.categories.forEach(function (cat) {
-                  var catNameInUrl = makeInputUrlFriendly(cat.name),
-                      catLink = '/search/' + catNameInUrl + '/' + queryTerm,
-                      catHtml = '<span class="category">' +
-                        '<a href="' + catLink + '">' + cat.name + '</a>' +
-                        //'<button class="close">' +
-                        '</span>';
-
-                  categories.append(catHtml);
-               });
-            }
-
-         });
+         }).done(handleAjaxResult);
       } else {
          results.html('');
       }

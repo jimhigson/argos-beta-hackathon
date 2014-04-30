831496530f4ed91d95fd76b697324832a8ab086f
Jim Higson <jim.higson@gmail.com>
2014-04-30 14:57:26 +0100

    include handlebars js in front end

--- jsSourcesList.js
+++ jsSourcesList.js
@@ -1,4 +1,5 @@
 module.exports = [
+   '/bower_components/handlebars/handlebars.js',
    '/bower_components/jquery/dist/jquery.js',
    '/bower_components/unveil/jquery.unveil.min.js',
    '/bower_components/sticky/jquery.sticky.js',

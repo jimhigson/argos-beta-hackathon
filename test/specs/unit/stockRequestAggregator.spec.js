e1bb406d8a63f2cb2d956a33293e95c4ae7c1bc5
Jim Higson <jim.higson@gmail.com>
2014-05-13 16:08:49 +0100

    inline some vars

--- test/specs/unit/stockRequestAggregator.spec.js
+++ test/specs/unit/stockRequestAggregator.spec.js
@@ -21,13 +21,11 @@ describe('providing a facility to check stock', function() {
    it( 'gives a single callback after making multiple requests', function() {
 
       stockApiRequestAggregator.setBatchSize(4);
-      
-      var partNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
       // for this test we don't care about the actual results contents - always give the same
       stockApiRequestorStub.callsArgWithAsync(2, [{partNumber:1, availability:'available'}]);
 
-      stockApiRequestAggregator.request(partNumbers, aStoreNumber, resultsCallback);
+      stockApiRequestAggregator.request([1,2,3,4,5,6,7,8,9,10,11,12], aStoreNumber, resultsCallback);
 
       waitsFor(function(){ return resultsCallback.called; }, 'the callback to have been called');
 
@@ -40,13 +38,11 @@ describe('providing a facility to check stock', function() {
    it( 'can aggregate into really small batches and combine the results', function() {
       
       stockApiRequestAggregator.setBatchSize(2);
-      
-      var partNumbers = [1,2,3];
 
       whenRequestingProducts([1,2]).theServiceReturns([{partNumber:1, availability:'available'}, {partNumber:2, availability:'out-of-stock'}]);
       whenRequestingProducts([3])  .theServiceReturns([{partNumber:3, availability:'available'}]);
 
-      stockApiRequestAggregator.request(partNumbers, aStoreNumber, resultsCallback);
+      stockApiRequestAggregator.request([1,2,3], aStoreNumber, resultsCallback);
       
       waitsFor(function(){ return resultsCallback.called; }, 'the callback to have been called');
       

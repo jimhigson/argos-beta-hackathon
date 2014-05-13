52cf9b79a5615ec5b3cfda0885bb1010f35c4fb2
Jim Higson <jim.higson@gmail.com>
2014-05-13 10:59:54 +0100

    refactor making requests to stock API to use handlebars instead of massive string concatting

--- src/stockApi/stock.js
+++ src/stockApi/stock.js
@@ -1,5 +1,8 @@
 var request = require('request'),
-    parseStockApiResponse = require('./parseStockApiResponse.js');
+    handlebars = require('handlebars'),
+    fs = require('fs'),
+    parseStockApiResponse = require('./parseStockApiResponse.js'),
+    requestXmlBodyTemplate = handlebars.compile( fs.readFileSync('src/stockApi/requestTemplate.handlebars', 'utf-8') );
 
 module.exports = function getStockInfo(req, res) {
 
@@ -15,32 +18,20 @@ module.exports = function getStockInfo(req, res) {
       });
    });
 
+   function requestXmlBody(partNumbers, storeNumber) {
+      return requestXmlBodyTemplate({
+         storeNumber: storeNumber,
+         partNumbers: partNumbers
+      });
+   }
+   
    function makeXMLRequestBody(partNumbers, storeNumber, callback) {
 
       //Start of XML request
-      var xmlRequest = '<?xml version="1.0" encoding="UTF-8"?><stk:Stock brand="argos" version="1"\
-         xsi:schemaLocation="http://schemas.homeretailgroup.com/stock stock-v1.xsd"\
-         xmlns:bsk="http://schemas.homeretailgroup.com/basket"\
-         xmlns:cmn="http://schemas.homeretailgroup.com/common"\
-         xmlns:loc="http://schemas.homeretailgroup.com/location"\
-         xmlns:stk="http://schemas.homeretailgroup.com/stock"\
-         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">\
-         <stk:LocationList>';
-
-      // Store to search
-      xmlRequest += '<loc:Location uri="http://api.homeretailgroup.com/location/argos/store/'+storeNumber+'"/></stk:LocationList><bsk:Basket version="1"><bsk:ItemList>';
-
-      // Loop and add items to request stock info for
-      for(var i = 0; i < partNumbers.length; ++i) {
-         xmlRequest += '<cmn:Item type="product" uri="http://api.homeretailgroup.com/product/argos/' + partNumbers[i] + '" id="' + partNumbers[i] + '"><cmn:Quantity type="requested">1</cmn:Quantity></cmn:Item>';
-      }
-      // End of XML request
-      xmlRequest += '</bsk:ItemList></bsk:Basket></stk:Stock>';
-
       request({
          url: 'https://api.homeretailgroup.com/stock/argos?apiKey=uk4tbngzceyxpwwvfcbtkvkj',
          method: 'POST',
-         body: xmlRequest
+         body: requestXmlBody(partNumbers, storeNumber)
       }, function(error, response) {
          callback(response.body);
       });
